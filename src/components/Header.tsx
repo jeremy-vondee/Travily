a0a8bf3fb0d9ab02a294ed6eda9a1d8b33174525
jeremy-vondee <solomonodonkor79@gmail.com>
2026-03-28 20:34:57 +0000

    update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,9 @@
-import type { FC } from "react"
 import { useState } from "react"
-import { buttonVariants } from "./ui/button"
-import { CircleQuestionMark, Plane } from "lucide-react"
+import { Button, buttonVariants } from "./ui/button"
+import { CircleQuestionMark, Ticket, CircleUserRound } from "lucide-react"
 import ModeToggle from "./ModeToggle"
 
-const Header: FC = () => {
+const Header = () => {
     const [isOpen, setIsOpen] = useState(false)
     const toggleMenu = () => {
         setIsOpen((prev) => !prev)
@@ -12,35 +11,26 @@ const Header: FC = () => {
     return (
         <>
             <nav className="bg-primary h-16 flex items-center pl-3 pr-3 justify-between">
-                <h2 className="text-white">Travily</h2>
+                <a href="/">
+                    <img src="/logo.svg" alt="Travily logo" />
+                </a>
                 {/* Desktop nav */}
                 <div className="hidden lg:flex justify-evenly items-center w-1/3">
                     <a
-                        href="/help"
-                        className="text-white inline-flex items-center gap-1 pl-1 h-10">
+                        href="/mybooking"
+                        className="text-white inline-flex items-center gap-1 pl-1 h-10"
+                    >
+                        <Ticket className="w-4 h-4 shrink-0 rotate-317 -translate-y-0.5" />
+                        <span className="leading-none">My Booking</span>
+                    </a>
+                    <a href="/help" className="text-white inline-flex items-center gap-1 pl-1 h-10">
                         <CircleQuestionMark className="w-4 h-4 shrink-0 " />
                         <span className="leading-none">Help</span>
                     </a>
-                    <a
-                        href="/mytrip"
-                        className="text-white inline-flex items-center gap-1 pl-1 h-10">
-                        <Plane className="w-4 h-4 shrink-0 rotate-317 -translate-y-0.5" />
-                        <span className="leading-none">My Trip</span>
-                    </a>
                     <ModeToggle />
-                    <a href="/login" className="text-white">
-                        Login
-                    </a>
-                    <a
-                        href="/signup"
-                        className={buttonVariants({
-                            variant: "outline",
-                            size: "sm",
-                            className:
-                                "bg-secondary text-secondary-foreground hover:bg-secondary/80 border-secondary",
-                        })}>
-                        Signup
-                    </a>
+                    <Button variant="outline" size="icon-lg" className="bg-primary border-0">
+                        <CircleUserRound className="[&_svg]:size-7 shrink-0 text-white" />
+                    </Button>
                 </div>
                 {/* Mobile controls */}
                 <div className="flex items-center gap-1 lg:hidden">
@@ -49,7 +39,8 @@ const Header: FC = () => {
                     <button
                         type="button"
                         onClick={toggleMenu}
-                        className="lg:hidden p-2 mr-1 rounded focus:outline-none relative w-10 h-8 flex flex-col justify-center">
+                        className="lg:hidden p-2 mr-1 rounded focus:outline-none relative w-10 h-8 flex flex-col justify-center"
+                    >
                         <span
                             className={[
                                 "hamburger-line w-6 h-0.5 bg-white rounded transition-all duration-300 origin-center mb-1.5",
@@ -76,32 +67,36 @@ const Header: FC = () => {
             <nav
                 id="mobile-menu"
                 className={[
-                    "lg:hidden bg-slate-900 border-bg-slate-800 transition-all duration-300",
-                    isOpen ? "block" : "hidden",
-                ].join(" ")}>
+                    "lg:hidden bg-slate-900 border-bg-slate-800 transition-all duration-300 flex-col items-start",
+                    isOpen ? "flex" : "hidden",
+                ].join(" ")}
+            >
                 <a
-                    href="help"
-                    className="block text-white px-5 py-4 text-lg border-b border-gray-700 hover:bg-gray-700 transition-colors">
-                    Help
+                    href="/help"
+                    className="ml-2 text-white inline-flex items-center gap-1 pl-1 h-10"
+                >
+                    <CircleQuestionMark className="w-4 h-4 shrink-0 " />
+                    <span className="leading-none">Help</span>
                 </a>
                 <a
                     href="/mytrip"
-                    className="block text-white px-5 py-4 text-lg border-b border-gray-700 hover:bg-gray-700 transition-colors">
-                    My Trip
+                    className="ml-2 text-white inline-flex items-center gap-1 pl-1 h-10"
+                >
+                    <Ticket className="w-4 h-4 shrink-0 rotate-317 -translate-y-0.5" />
+                    <span className="leading-none">My Trip</span>
                 </a>
-                <a
-                    href="login"
-                    className="block text-white px-5 py-4 text-lg border-b border-gray-700 hover:bg-gray-700 transition-colors">
+                <a href="/login" className="ml-3 text-white">
                     Login
                 </a>
                 <a
-                    href="signup"
+                    href="/signup"
                     className={buttonVariants({
-                        variant: "outline",
+                        variant: "default",
                         size: "sm",
                         className:
-                            "mx-5 mt-4 mb-4 bg-secondary text-secondary-foreground hover:bg-secondary/80 border-secondary",
-                    })}>
+                            "mx-5 mt-4 mb-4 bg-secondary text-secondary-foreground hover:bg-secondary/80 border-secondary dark:bg-white dark:text-primay dark:hover:bg-white",
+                    })}
+                >
                     Signup
                 </a>
             </nav>
