9fafa12faf15cc8092ce2e35212fca6dd4d9a340
jeremy-vondee <solomonodonkor79@gmail.com>
2026-04-25 19:48:01 +0100

    update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,8 +1,31 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string
-console.log("supabaseUrl:", supabaseUrl)
-console.log("supabaseAnonKey:", supabaseAnonKey)
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL as string
+const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY as string
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+    auth: { flowType: "implicit" },
+})
+
+export async function sendOtp(email: string) {
+    const { data, error } = await supabase.auth.signInWithOtp({
+        email,
+        options: {
+            shouldCreateUser: true,
+        },
+    })
+
+    return { data, error }
+}
+
+export async function sendMagicLink(email: string) {
+    const { data, error } = await supabase.auth.signInWithOtp({
+        email,
+        options: {
+            shouldCreateUser: true,
+            emailRedirectTo: `${window.location.origin}/`,
+        },
+    })
+
+    return { data, error }
+}
