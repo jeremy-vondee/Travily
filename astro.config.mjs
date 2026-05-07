2bb2efc65540a2f6d32dc3030e453eaaceff5e50
jeremy-vondee <solomonodonkor79@gmail.com>
2026-05-07 14:59:32 +0100

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,22 @@
 // @ts-check
 import { defineConfig } from "astro/config"
-
+import path from "path"
 import tailwindcss from "@tailwindcss/vite"
-
 import react from "@astrojs/react"
 
-// https://astro.build/config
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
+        resolve: {
+            alias: {
+                "@": path.resolve("./src"),
+                "@/components": path.resolve("./src/components"),
+                "@/lib": path.resolve("./src/lib"),
+                "@/context": path.resolve("./src/context"),
+                "@/hooks": path.resolve("./src/hooks"),
+                "@/utils": path.resolve("./src/utils"),
+            },
+        },
     },
-
     integrations: [react()],
 })
